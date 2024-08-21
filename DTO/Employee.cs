34480bf55e8bce6b181e4da8180755c0478ff4f5
shubhampatel8982 <155613423+shubhampatel8982@users.noreply.github.com>
2024-08-21 16:14:49 +0530

    Add files via upload

---
+++ DTO/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace DT0
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public int? Salary { get; set; }
+        public string Dept { get; set; }
+        public string Password { get; set; }
+    }
+}
+
